a58e47734319155842c276bd1321f5df3819d9b3
Jack <JackAtGitHub@gmail.com>
2020-03-11 16:42:01 +0000

    Basic blocking functionality implemented.

--- app/src/main/java/com/example/chatapplication/BlocklistActivity.kt
+++ app/src/main/java/com/example/chatapplication/BlocklistActivity.kt
@@ -2,11 +2,17 @@ package com.example.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_blocklist.*
 
 class BlocklistActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blocklist)
+
+        supportActionBar?.title = "Blocked Users"
+
+        recyclerBlocklist.layoutManager = LinearLayoutManager(this)
     }
 }
